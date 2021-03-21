defb762b431e47935eb264ffe918b66c18619833
Aythami de Armas <aydearcas@gmail.com>
2021-03-21 12:17:03 +0000

    Initial Commit. Proyecto creado con Vue CLI, incluye Tailwind, Axios, LoDash, ChartJS, Jest y TestingLibrary

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,21 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+     semi: ["error", "always"],
+     quotes: ["error", "double", { allowTemplateLiterals: true }],
+     camelcase: ["error"],
+    "capitalized-comments": ["error"],
+    "no-unused-vars": ["warn"],
+    "max-lines-per-function": ["error", 60],
+    "max-nested-callbacks": ["error", 5],
+    "max-params": ["error", 10],
+    "max-depth": ["error", 5],
+    "no-useless-catch": ["error"],
+    "vars-on-top": ["warn"],
+    "no-unreachable": ["error"],
+    "no-extra-semi": ["error"],
+    "no-extra-parens": ["error"]
   }
 }
+
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1 class="text-5xl text-pink-500 font-bold">{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -32,11 +32,11 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
     msg: String
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
