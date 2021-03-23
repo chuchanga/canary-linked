ce656a888fa04a0982af8caae677eafbcf271bf8
Aythami de Armas <aydearcas@gmail.com>
2021-03-23 14:35:48 +0000

    Cambio de la forma de poner los colores en el tailwind.config.js y comentario de la forma antigua

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     fontFamily: {
       sans: ["Nunito"]
-    },
+    }, /*,
     backgroundColor: theme => ({
       ...theme("colors"),
       cyberyellow: "#FAD000",
@@ -16,9 +16,21 @@ module.exports = {
       capriblue: "#5CCBFF",
       davysgray: "#4D4D4D",
       richblack: "#0A0C09"
-    }),
-    extend: {}
-  },
+    }), /* De esta forma solo coge los colores para los fondos, pero no para texto, por ejemplo */
+    extend: {
+      colors: {
+        cyberyellow: "#FAD000",
+        minionyellow: "#FFE45C",
+        goldenrod: "#E6A503",
+        culturedwhite: "#F3F3F3",
+        honolulublue: "#0070A3",
+        bluejeans: "#00A8F5",
+        capriblue: "#5CCBFF",
+        davysgray: "#4D4D4D",
+        richblack: "#0A0C09"
+      }
+    }
+  }, /* De esta forma puedes coger los colores para todo */
   variants: {
     extend: {}
   },
