5c6d6eaca2d393e9287555ef695777613ed6edb4
Saúl Abreu <saulabreu@outlook.es>
2021-03-24 18:50:09 +0000

    navegacion2

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,15 @@
 import Vue from "vue";
 import Login from "../view/Login.vue";
 import Header from "../view/Header.vue";
+import Signup from "../view/Signup.vue";
 import VueRouter from "vue-router";
 
 Vue.use(VueRouter);
 
 const routes = [
   { path: "/login", component: Login },
-  { path: "/header", component: Header }
+  { path: "/header", component: Header },
+  { path: "/signup", component: Signup }
 ];
 
 const router = new VueRouter({
