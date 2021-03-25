11c1a9ab0551a1135df477389744fcfcc7324f7c
Saúl Abreu <saulabreu@outlook.es>
2021-03-25 11:11:01 +0000

    No hay cambios

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <template>
   <div id="app">
+     <Header/>
     <router-view> </router-view>
   </div>
 </template>
 
 <script>
-
+// Import Header from "./views/Header.vue";
 export default {
   name: "App",
   components: {}
