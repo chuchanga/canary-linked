0550554c00c806f7d005ba84da58c3ed41011c23
Aythami de Armas <aydearcas@gmail.com>
2021-03-30 14:44:28 +0100

    Añadida la configuración básica de firebase-firestore en el proyecto

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "AIzaSyBj4Z-rcjquT5-LkbVjNJ9gJQrjqvIhLdc",
+  authDomain: "canarylinked.firebaseapp.com",
+  projectId: "canarylinked",
+  storageBucket: "canarylinked.appspot.com",
+  messagingSenderId: "747510999077",
+  appId: "1:747510999077:web:7eabf7fbb62dcfbe1137cc",
+  measurementId: "G-V3L131RGK5"
+};
