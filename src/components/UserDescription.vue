98d4d619861583fbf263ab8ac6942a449bd30ed5
Aythami de Armas <aydearcas@gmail.com>
2021-03-30 16:26:15 +0100

    Establecida conexión con la base de datos de Firebase y acceso a los datos como ejemplo en el perfil del usuario, /profile

--- src/components/UserDescription.vue
+++ src/components/UserDescription.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="user-info w-72 h-auto m-4 p-4 shadow-lg flex flex-col">
     <div class="user-image mb-8 bg-hero h-48 w-48 rounded-full self-center"></div>
-    <div class="user-name ml-2 h-1/6 text-left">Aythami de Armas Castellano</div>
-    <div class="user-id mt-1 ml-2 h-1/6 text-left text-gray-400">@aydearcas</div>
-    <div class="user-brief-description ml-2 mt-8 h-1/6 text-left text-xs">Antropólogo Social y Web Developer. Nacido en Cardones. Estudié Antropología Social y Cultural y Ahora estoy en el paro, por eso me dedico a pintarrajear páginas webs jeje xd</div>
+    <div class="user-name ml-2 h-1/6 text-left" v-for="user in users" v-bind:key="user.email">{{user.name}} {{user.surname}}</div>
+    <div class="user-id mt-1 ml-2 h-1/6 text-left text-gray-400" v-for="user in users" v-bind:key="user.email">@{{user.usertag}}</div>
+    <div class="user-brief-description ml-2 mt-8 h-1/6 text-left text-xs" v-for="user in users" v-bind:key="user.email">{{ user.brief }}</div>
     <div class="user-contact ml-2 mt-8 h-1/6">
-      <div class="contact-location h-1/6 text-left text-sm">
-        <i class="text-davysgray fas fa-map-marker-alt mr-4"></i>Location
+      <div class="contact-location h-1/6 text-left text-sm" v-for="user in users" v-bind:key="user.email">
+        <i class="text-davysgray fas fa-map-marker-alt mr-4"></i>{{ user.location }}
        </div>
-      <div class="contact-mail h-1/6 text-left text-sm">
-        <i class="text-davysgray fas fa-envelope mr-2"></i> mail
+      <div class="contact-mail h-1/6 text-left text-sm" v-for="user in users" v-bind:key="user.email">
+        <i class="text-davysgray fas fa-envelope mr-2"></i> {{user.email}}
        </div>
-      <div class="contact-web h-1/6 text-left text-sm">
-        <i class=" text-davysgray fas fa-pager mr-2"></i> Webpage
+      <div class="contact-web h-1/6 text-left text-sm" v-for="user in users" v-bind:key="user.email">
+        <i class=" text-davysgray fas fa-pager mr-2"></i>
+        {{ user.website }}
        </div>
     </div>
     <Button class="mt-8 mb-6" text="Editar Perfil" />
@@ -21,9 +22,32 @@
 
 <script>
 import Button from "../components/ButtonCard.vue";
-
+import db from "./firebaseInit.js";
 export default {
   name: "UserDescription",
+  data () {
+    return {
+      users: []
+    };
+  },
+  created () {
+    db.collection("users").get().then(querySnapshot => {
+      querySnapshot.forEach(doc => {
+        const data = {
+          usertag: doc.data().userTag,
+          email: doc.data().email,
+          name: doc.data().userName,
+          surname: doc.data().userSurname,
+          brief: doc.data().userBrief,
+          location: doc.data().userLocation,
+          website: doc.data().userWeb
+
+        };
+        this.users.push(data);
+      });
+    });
+  },
+
   components: {
     Button,
   }
