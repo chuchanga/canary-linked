22a00c3f03a56cca6b1111caa8fe35382d8c294c
Saúl Abreu <saulabreu@outlook.es>
2021-03-31 21:44:28 +0100

    Se mejoró la animación

--- src/views/Training.vue
+++ src/views/Training.vue
@@ -2,8 +2,12 @@
   <div class="container m-auto justify-items-center" >
     <div class="flex flex-row justify-items-center">
     <div v-for="card in arrayTraining" :key="card.id">
-      <div v-if="card.show">
-        <Card :title = "card.title" :description = "card.show" :id="card.id" :text="card.text"/>
+      <div :id="card.id" v-if="card.show" class="flip-vertical-right"> {{card.id}}
+        <div v-if="card.id == getChoice">
+        <div v-if="arrayTraining[getChoice].pos == true">{{animate()}}</div>
+        <div v-if="arrayTraining[getChoice].pos == false">{{reverse()}}</div>
+      </div>
+        <Card :title = "card.title" :description = "card.show" :id = "card.id" :text = "card.text"/>
       </div>
     </div>
     </div>
@@ -14,7 +18,7 @@
 <script>
 import Pagination from "../components/Pagination.vue";
 import Card from "../components/Card.vue";
-import { mapState } from "vuex";
+import { mapState, mapGetters } from "vuex";
 
 export default {
   name: "Training",
@@ -24,6 +28,53 @@ export default {
   },
   computed: {
     ...mapState("pagination", ["arrayTraining"]),
+    ...mapGetters("animation", ["getChoice"]),
   },
+  methods: {
+    animate() {
+      if (document.getElementById(this.getChoice) != null) {
+        const element = document.getElementById(this.getChoice);
+        element.classList.toggle("flip-vertical-right");
+        element.classList.toggle("flip-vertical-left");
+        console.log(element);
+      }
+    },
+    reverse() {
+      if (document.getElementById(this.getChoice) != null) {
+        const element = document.getElementById(this.getChoice);
+        element.classList.toggle("flip-vertical-left");
+        element.classList.toggle("flip-vertical-right");
+        console.log(element);
+      }
+    },
+  }
 };
 </script>
+
+<style>
+.flip-vertical-right {
+  animation: flip-vertical-right 0.5s cubic-bezier(0.455, 0.030, 0.515, 0.955);
+}
+
+.flip-vertical-left {
+  animation: flip-vertical-left 0.5s cubic-bezier(0.455, 0.030, 0.515, 0.955);
+}
+
+@keyframes flip-vertical-right {
+  0% {
+    transform: rotateY(0);
+  }
+  100% {
+    transform: rotateY(180deg);
+  }
+}
+
+@keyframes flip-vertical-left {
+  0% {
+    transform: rotateY(0);
+  }
+  100% {
+    transform: rotateY(-180deg);
+  }
+}
+</style>
