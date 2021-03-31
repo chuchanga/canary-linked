66ae67ad0cffacc351b608f8f176895b8457c5cd
Aythami de Armas <aydearcas@gmail.com>
2021-03-31 01:00:06 +0100

    Algunos cambios en el perfil de usuario, sobre todo con lo referente a firebase

--- src/components/UserDescription.vue
+++ src/components/UserDescription.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="user-info w-72 h-auto m-4 p-4 shadow-lg flex flex-col">
     <div class="user-image mb-8 bg-hero h-48 w-48 rounded-full self-center"></div>
-    <div class="user-name ml-2 h-1/6 text-left" v-for="user in users" v-bind:key="user.email">{{user.name}} {{user.surname}}</div>
-    <div class="user-id mt-1 ml-2 h-1/6 text-left text-gray-400" v-for="user in users" v-bind:key="user.email">@{{user.usertag}}</div>
-    <div class="user-brief-description ml-2 mt-8 h-1/6 text-left text-xs" v-for="user in users" v-bind:key="user.email">{{ user.brief }}</div>
+    <div class="user-name ml-2 h-1/6 text-left">{{users[0].name}} {{users[0].surname}}</div>
+    <div class="user-id mt-1 ml-2 h-1/6 text-left text-gray-400">@{{users[0].usertag}}</div>
+    <div class="user-brief-description ml-2 mt-8 h-1/6 text-left text-xs">{{ users[0].brief }}</div>
     <div class="user-contact ml-2 mt-8 h-1/6">
-      <div class="contact-location h-1/6 text-left text-sm" v-for="user in users" v-bind:key="user.email">
-        <i class="text-davysgray fas fa-map-marker-alt mr-4"></i>{{ user.location }}
+      <div class="contact-location h-1/6 text-left text-sm">
+        <i class="text-davysgray fas fa-map-marker-alt mr-4"></i>{{ users[0].location }}
        </div>
-      <div class="contact-mail h-1/6 text-left text-sm" v-for="user in users" v-bind:key="user.email">
-        <i class="text-davysgray fas fa-envelope mr-2"></i> {{user.email}}
+      <div class="contact-mail h-1/6 text-left text-sm">
+        <i class="text-davysgray fas fa-envelope mr-2"></i> {{users[0].email}}
        </div>
-      <div class="contact-web h-1/6 text-left text-sm" v-for="user in users" v-bind:key="user.email">
+      <div class="contact-web h-1/6 text-left text-sm">
         <i class=" text-davysgray fas fa-pager mr-2"></i>
-        {{ user.website }}
+        {{ users[0].website }}
        </div>
     </div>
     <Button class="mt-8 mb-6" text="Editar Perfil" />
