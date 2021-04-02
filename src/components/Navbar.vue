75f68783a3554254ac108e71405b89b16aed2bc1
Aythami de Armas <aydearcas@gmail.com>
2021-04-02 19:54:05 +0100

    Modificación del navbar para adaptar mejor el control de la sesión de usuario

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,36 +1,47 @@
 <template>
   <div class="content">
-  <div class="px-64 py-3 bg-richblack text-white">
+  <div class="px-56 py-1 bg-richblack text-white">
     <div class="flex justify-between items-center">
       <div class="flex flex-col space-x-1">
-        <router-link to="/home"><img class="-mb-2 h-24 w-24" src="../assets/logo.svg" alt="logo">
-        <div class="text-xs uppercase font-bold tracking-wide text-cyberyellow">Canary Linked</div>
+        <router-link to="/home"><img class="-mb-3 -mt-3 h-24 w-24" src="../assets/logo.svg" alt="logo">
+        <div class="text-xs uppercase font-bold tracking-wide text-cyberyellow -mt-3">Canary Linked</div>
         </router-link>
       </div>
-            <div>
+            <div class="hover:text-cyberyellow text-lg">
+              <router-link to="/training">Tablón</router-link>
+            </div>
+            <div class="hover:text-cyberyellow text-lg">
               <router-link to="/training">Noticias</router-link>
             </div>
-            <div>
+            <div class="hover:text-cyberyellow text-lg">
               <router-link to="/contact">Contacto</router-link>
             </div>
-            <div>
+            <div class="hover:text-cyberyellow text-lg">
               <router-link to="/about">Sobre Nosotros</router-link>
             </div>
-            <div v-if="isLoggedIn">
-              <router-link to="/profile">Perfil</router-link>
+             <div class="container w-28">
+            <div class="relative text-left inline-flex flex-col">
+              <button @click="open = !open" class="mt-2 p-3 w-32 hover:text-cyberyellow bg-richblack items-center text-culturedwhite focus:outline-none rounded-full">
+                  <i class="far fa-user fa-3x rounded-full self-center "></i>
+              </button>
+              <span @click="open = !open" v-if="isLoggedIn" class="cursor-pointer block text-culturedwhite text-center text-xs -mt-2">Sesión iniciada como {{currentUser}}</span>
+              <span @click="open = !open" v-if="!isLoggedIn" class="cursor-pointer block text-culturedwhite text-center self-center text-xs mb-2">No ha iniciado sesión</span>
+              <div v-if="open" class="absolute mt-24 bg-richblack w-32">
+                <div v-if="!isLoggedIn" class="cursor-pointer dropdown-item block p-2 border-b text-culturedwhite border-davysgray  hover:text-cyberyellow">
+                  <router-link to="/login">Iniciar Sesión</router-link>
+                </div>
+                <div v-if="!isLoggedIn" class="cursor-pointer dropdown-item block p-2 text-culturedwhite hover:text-cyberyellow">
+                  <router-link to="/signup">Regístrate</router-link>
+                </div>
+                <div v-if="isLoggedIn" class="cursor-pointer dropdown-item block p-2 border-b text-culturedwhite border-davysgray hover:bg-cyberyellow hover:border-richblack hover:text-richblack">
+                  <router-link to="/profile">Perfil</router-link>
+                </div>
+                 <div v-if="isLoggedIn">
+                  <div class="cursor-pointer dropdown-item block p-2  text-culturedwhite hover:bg-cyberyellow hover:border-richblack hover:text-richblack" @click="logout">Logout</div>
+                </div>
+              </div>
             </div>
-             <div v-if="!isLoggedIn">
-              <router-link to="/login">Inicia Sesión</router-link>
-            </div>
-            <div v-if="!isLoggedIn">
-              <router-link to="/signup">Regístrate</router-link>
-            </div>
-            <div v-if="isLoggedIn">
-              <button class="bg-richblack hover:bg-cyberyellow text-center w-full text-white hover:text-richblack rounded leading-tight md:text-base font-sans" @click="logout">Logout</button>
-            </div>
-            <div>
-              <DropdownMenu />
-            </div>
+  </div>
     </div>
   </div>
   </div>
@@ -39,16 +50,15 @@
 <script>
 import firebase from "firebase/app";
 import "firebase/auth";
-import DropdownMenu from "./DropdownMenu.vue";
 export default {
   name: "Navbar",
   components: {
-    DropdownMenu
   },
   data() {
     return {
       isLoggedIn: false,
       currentUser: false,
+      open: false
     };
   },
   created() {
