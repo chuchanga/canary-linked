c1e8017f85438eb0eb0b8b6777e9b35a60bb3cb2
Saúl Abreu <saulabreu@outlook.es>
2021-04-06 21:05:04 +0100

    Cambios en orientacion laboral

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-col items-center my-12">
-    <p class="justify-items-center">Estás en la página: {{ currentPage }}</p>
+    <!--p class="justify-items-center">Estás en la página: {{ currentPage }}</p-->
     <div class="flex text-black">
       <div class="flex h-12 font-medium rounded-full bg-cyberyellow">
         <div
