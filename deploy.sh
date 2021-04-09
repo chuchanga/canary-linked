a571a5c02e489a18e7c2ea46bec03cc946bc4be7
Aythami de Armas <aydearcas@gmail.com>
2021-04-09 12:32:37 +0100

    Modificados del script, cambié canarylinked por canary-linked

--- deploy.sh
+++ deploy.sh
@@ -19,6 +19,6 @@ git commit -m 'deploy'
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
-git push -f git@github.com:chuchanga/canarylinked.git master:gh-pages
+git push -f git@github.com:chuchanga/canary-linked.git master:gh-pages
 
 cd -
