281572f05312272faad6e8be3f8737ba96b3c8e8
Aythami de Armas <aydearcas@gmail.com>
2021-04-17 11:29:06 +0100

    Cambio en Script de deploy para que añada el CNAME

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ npm run build
 cd dist && cp index.html 404.html
 
 # if you are deploying to a custom domain
-echo 'app.canarylinked.com' > CNAME
+echo 'app.canarylinked.es' > CNAME
 
 git init
 git add -A
